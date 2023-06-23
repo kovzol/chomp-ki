fd64fee4d653e3ffbdddbeb635f33c9e3b7c443b
Zoltán Kovács <zoltan@geogebra.org>
2023-06-23 17:06:45 +0200

    Cleanup (während der LVA)

--- chomp.py
+++ chomp.py
@@ -3,7 +3,7 @@ n = 36
 def teiler(k):
     """
     Diese Funktion berechnet alle positiven Teiler der ganzen Zahl k.
-    Sein Rückgabewert ist eine Liste mit den Teilern.
+    Ihr Rückgabewert ist eine Liste mit den Teilern.
     """
     t = []
     for i in range(1,int(k/2)+1):
@@ -34,12 +34,11 @@ def mögliche_züge(s):
             if z%t == 0 and t in erlaubte_züge:
                 erlaubte_züge.remove(t)
     möglich = []
-    for t in alle_teiler:
-        if t in erlaubte_züge:
-            neue_situation = s.copy()
-            neue_situation.append(t)
-            möglich.append(neue_situation)
-            pfeile.append([s, neue_situation])
+    for t in erlaubte_züge:
+        neue_situation = s.copy()
+        neue_situation.append(t)
+        möglich.append(neue_situation)
+        pfeile.append([s, neue_situation])
     return möglich
 
 def speichere_züge_als_punkte(situationen):
@@ -49,8 +48,7 @@ def speichere_züge_als_punkte(situationen):
     for s in situationen:
         mz = mögliche_züge(s)
         for z in mz:
-            if not (z in punkte):
-                punkte.append(z)
+            punkte.append(z)
 
 def graphviz_output(dateiname):
     """
@@ -67,16 +65,16 @@ def graphviz_output(dateiname):
     d.write("}\n")
     d.close()
 
-def letzte_züge_rot():
+def letzte_situationen_rot():
     """
-    Färbt alle letzten Züge rot.
+    Färbt alle letzten Situationen rot.
     """
-    letzte_züge = punkte.copy()
+    letzte_situationen = punkte.copy()
     for s in punkte:
         for ü in pfeile:
-            if ü[0] == s and s in letzte_züge:
-                letzte_züge.remove(s)
-    for z in letzte_züge:
+            if ü[0] == s and s in letzte_situationen:
+                letzte_situationen.remove(s)
+    for z in letzte_situationen:
         farben[tuple(z)] = 'red'
 
 def vorteilhafte_situationen_grün():
@@ -87,11 +85,11 @@ def vorteilhafte_situationen_grün():
         grün_möglich = True
         for ü in pfeile:
             if ü[0] == s:
-                nächster_zug = tuple(ü[1])
-                if not (nächster_zug in farben):
+                nächste_situation = tuple(ü[1])
+                if not (nächste_situation in farben):
                     grün_möglich = False # Es gibt keine Information über den nächsten Zug
                 else:
-                    if farben[nächster_zug] == 'green': # s kann nicht vorteilhaft sein
+                    if farben[nächste_situation] == 'green': # s kann nicht vorteilhaft sein
                         grün_möglich = False
         if not tuple(s) in farben and grün_möglich:
             farben[tuple(s)] = 'green'
@@ -105,8 +103,8 @@ def unvorteilhafte_situationen_rosa():
         rosa = False
         for ü in pfeile:
             if ü[0] == s:
-                nächster_zug = tuple(ü[1])
-                if nächster_zug in farben and farben[nächster_zug] == 'green': # s ist nicht vorteilhaft
+                nächste_situation = tuple(ü[1])
+                if nächste_situation in farben and farben[nächste_situation] == 'green': # s ist nicht vorteilhaft
                     rosa = True
         if not tuple(s) in farben and rosa:
             farben[tuple(s)] = 'pink'
@@ -115,7 +113,6 @@ def alle_situationen_überprüft():
     """
     Entscheidet ob alle Situationen schon überprüft (gefärbt) wurden.
     """
-    fertig = True
     for s in punkte:
         if not(tuple(s)) in farben:
             return False
@@ -123,8 +120,8 @@ def alle_situationen_überprüft():
 
 speichere_züge_als_punkte([[]])
 speichere_züge_als_punkte(punkte)
-letzte_züge_rot()
+letzte_situationen_rot()
 while not alle_situationen_überprüft():
-   vorteilhafte_situationen_grün()
-   unvorteilhafte_situationen_rosa()
+    vorteilhafte_situationen_grün()
+    unvorteilhafte_situationen_rosa()
 graphviz_output("chomp.gv")
